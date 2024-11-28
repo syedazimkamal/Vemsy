b10fe7c2f651355bc3d374da2ec6a319500a1eee
Syed Azim <46717107+syedazimkamal@users.noreply.github.com>
2024-11-28 12:40:16 +0530

    settings.py

--- VES/settings.py
+++ VES/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--wsuy7!!52k)pp&#$_)-x2!cw9^#0(dq61*_dv6-=5+qt_sg&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
